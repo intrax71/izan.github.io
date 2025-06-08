eb9b43d29c73647e7477c61f9b1a4c970db2c57b
intrax71 <107364803+intrax71@users.noreply.github.com>
2025-06-08 21:14:51 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,9 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-   site: 'https://izan.github.io',
-  base: 'my-repo',
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
   integrations: [react()]
 });
